6f869a19e0940d4ddbb181312421a7ed924474c5
george <george.benedict@student.moringaschool.com>
2025-05-27 17:52:22 +0300

    model setup

---
+++ Lib/models/author.py
@@ -0,0 +1,45 @@
+from lib.db.connection import get_connection
+
+class Author:
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+        self.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    def articles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self.id,))
+        return cursor.fetchall()
+
+    def magazines(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.* FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        return cursor.fetchall()
+
+    def add_article(self, magazine, title):
+        from lib.models.article import Article
+        article = Article(title=title, author_id=self.id, magazine_id=magazine.id)
+        article.save()
+
+    def topic_areas(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT m.category FROM magazines m
+            JOIN articles a ON m.id = a.magazine_id
+            WHERE a.author_id = ?
+        """, (self.id,))
+        return [row['category'] for row in cursor.fetchall()]
